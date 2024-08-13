6853d18153c9de014f4497de2782b802d527873f
kazeneza-zephilin <kzephilin2@gmail.com>
2024-08-13 19:07:35 +0200

    complying to pep8 style

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -7,10 +7,10 @@ def count_words(subreddit, word_list, instances={}, after="", count=0):
     """Prints counts of given words found in hot posts of a given subreddit.
 
     Args:
-        subreddit (str): The subreddit to search.
-        word_list (list): The list of words to search for in post titles.
+        subreddit (str): The subreddit
+        word_list (list): The list of words to search
         instances (obj): Key/value pairs of words/counts.
-        after (str): The parameter for the next page of the API results.
+        after (str): The parameter for the next page..
         count (int): The parameter of results matched thus far.
     """
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
